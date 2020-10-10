934e299cf9c4b6563942cff8c1e4c96c9ab1b926
Christopher Göbel <christopher.goebel.93@gmail.com>
2020-10-10 12:28:13 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace statmathPostgreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
