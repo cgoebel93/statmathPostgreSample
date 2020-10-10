c6b84a350ad9dcf420dc4ff529a95a4daadebe52
Christopher Göbel <christopher.goebel.93@gmail.com>
2020-10-10 14:45:42 +0200

    update dockerfiles

---
+++ Database/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace statmathPostgreSample.Database
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=testdb;Username=testuser;Password=testpwd");
+    }
+
+    public class Blog
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+
+    }
+}
